82ef2379be2bffbd57bacbea7d7486c25571b91f
Ahmed chikh salah <ahmedchikhsalah4@gmail.com>
2024-12-02 18:29:54 +0100

    add cross origin to the controllers

--- src/main/java/com/example/miniprojet/controller/ChoixController.java
+++ src/main/java/com/example/miniprojet/controller/ChoixController.java
@@ -3,14 +3,12 @@ package com.example.miniprojet.controller;
 import com.example.miniprojet.model.Choix;
 import com.example.miniprojet.service.ChoixService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/choice")
 public class ChoixController {
     @Autowired
